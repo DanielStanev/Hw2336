c2a39296f5b10c8aac6c5db5dc0a2d7f77040310
DanielStanev <Daniel.Stanev@protonmail.com>
2021-07-18 17:22:45 -0500

    added person class

---
+++ Hw4/src/Hw4_Ex8.java
@@ -0,0 +1,7 @@
+public class Hw4_Ex8 {
+    public static void main(String[] x) {
+        Student p1 = new Student("Didko", "aoe", "<EU", "<AOE", "aoeu");
+
+        System.out.print(p1.toString());
+    }
+}
--- Hw4/src/Person.java
+++ Hw4/src/Person.java
@@ -1,2 +1,43 @@
 public class Person {
+    String name, address, phone, email;
+
+    Person() {}
+    Person(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Class: " + getClass().getName() + "\nName: " + name;
+    }
+}
+class Student extends Person {
+    private final String classStatus;
+
+    Student(String name, String address, String phone, String email, String classStatus) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.classStatus = classStatus;
+    }
+    Student(String classStatus) {
+        this.classStatus = classStatus;
+    }
+
+    //public "Student: " + name;
+}
+
+class Employee extends Person {
+
+}
+
+class Staff extends Employee {
+
+}
+
+class Faculty extends Employee {
+
 }
