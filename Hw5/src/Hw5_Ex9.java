89554c1ee06e9a2127c8aaa1c659928b4c3d329e
DanielStanev <Daniel.Stanev@protonmail.com>
2021-07-22 22:10:32 -0500

    Completed

--- Hw5/src/Hw5_Ex9.java
+++ Hw5/src/Hw5_Ex9.java
@@ -1,7 +1,7 @@
 public class Hw5_Ex9 {
     public static void main(String[] x) {
-        java.util.Scanner input = new java.util.Scanner(System.in);
         
+
     }
     public static <E extends Comparable<E>> int linearSearch(E[] list, E key) {
         for (int i = 0; i < list.length; i++)
